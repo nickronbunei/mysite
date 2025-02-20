41d4d5dc731ee67ca9789bd988b69d772b35d50d
nickronkibunei <nkibunei@gmail.com>
2025-02-20 21:30:25 +0300

    Templates added

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Hello World")
+from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home)
